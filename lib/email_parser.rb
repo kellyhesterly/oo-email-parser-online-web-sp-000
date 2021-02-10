0152a8ffbafca42aa8b35585ff5867b22ce0dade
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-02-10 23:28:08 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -12,7 +12,7 @@ def initialize(email)
 end
 
 def parse
-  email.split.collect {|e| e.strip.join(' ')}
+  email.split.collect {|e| e }
   # email.split.collect {|e| e.strip.split(" , ")}
   binding.pry
 end
